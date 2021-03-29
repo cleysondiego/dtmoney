8a29e5cc9c096c0e0a0b444542e553f68f95ebf3
Cleyson Silva <cleyson.dasilva@dieboldnixdorf.com>
2021-03-29 18:33:18 -0300

    Aulas: Introdução à contextos e A Context API no React

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,7 @@
 import { useState } from 'react';
 import Modal from 'react-modal';
 
+import { TransactionsContext } from './TransactionsContext';
 import { Dashboard } from "./components/Dashboard";
 import { Header } from "./components/Header";
 import { NewTransactionModal } from './components/NewTransactionModal';
@@ -21,7 +22,7 @@ export function App() {
   }
 
   return (
-    <>
+    <TransactionsContext.Provider value={[]}>
       <Header onOpenNewTransactionModal={handleOpenNewTransctionModal}/>
       <Dashboard />
 
@@ -31,6 +32,6 @@ export function App() {
       />
 
       <GlobalStyle />
-    </>
+    </TransactionsContext.Provider>
   );
 }
